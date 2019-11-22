1ab678538fb0b8473451538dea316ac5c954add1
nandhakumar <nandhakumar7thjuly@gmail.com>
2019-11-22 09:49:18 +0530

    Modifying initial files

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, '/', (req, res) => {
-    res.send('You are listenning to port 3000');
+app.get('/', (req, res) => {
+    res.send('this is working');
+});
+
+app.listen(3000, () => {
+    console.log('You are listenning to port 3000');
 });
