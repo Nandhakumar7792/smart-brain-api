00d21e245c1e7a874120098a569bf770d6717efe
nandhakumar <nandhakumar7thjuly@gmail.com>
2019-12-01 15:11:39 +0530

    Removing old database references

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send(`it's working`);
 });
 
 app.post('/signin', signin.handleSignin(db, bcrypt, saltRounds)); //Advanced concept re, res is passed directly we don't need to pass it
